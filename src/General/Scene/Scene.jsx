9093a9cd6a848e70f14638afeacf3eeb8db7f88f
AtNovember <atnovemb@gmail.com>
2017-01-24 21:12:38 +0400

    fix scene bg

--- src/General/Scene/Scene.jsx
+++ src/General/Scene/Scene.jsx
@@ -16,19 +16,20 @@ export default class Scene extends Component {
     // bubbles
     const wrap = this.props.wrap ? 'wrap' : ''
     const movie__card = this.props.movie__card ? 'movie__card' : ''
-    const bg = this.props.background || this.props.bg || ''
+    const bg = this.props.background || this.props.bg || '';
+
+    console.log('bg', bg);
     const scene = <div
       styleName={'Scene ' + wrap + ' ' + movie__card}
-      style={{backgroundImage:'url(' + bg + ');'}}
+      style={{backgroundImage: 'url('+bg+')' }}
     >
       {this.props.children}
     </div>
 
     let wd = this.props.width || this.props.wd || this.props.wide || 12
     // console.log(wd);
-    return <Col sm={wd} lg={wd}>
+    return <Col xs={wd} sm={wd} md={wd} lg={wd}>
       {scene}
     </Col>
-
   }
 }
